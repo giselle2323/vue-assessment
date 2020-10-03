2ab2bdf3ce6e5f147b654d736374651a719b4552
giselle2323 <yakubuaminat94@gmail.com>
2020-10-03 23:59:51 +0100

    chore: added reset css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link href="css/reset.css" rel="stylesheet" >
         <link href="{{ mix('css/app.css') }}" rel="stylesheet" >
     </head>
     <body>
