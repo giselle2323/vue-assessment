78759fd37494e8e26e5166ef3faebb7605f0643b
giselle2323 <yakubuaminat94@gmail.com>
2020-10-03 23:59:08 +0100

    feat: Employee Container

--- resources/components/EmployeesContainer.vue
+++ resources/components/EmployeesContainer.vue
@@ -1,16 +1,18 @@
 <template>
   <section class="employee-container">
     <header class="employee-container__header">
-      <button class="employee_container__btn primary__btn">Add Employee</button>
+      <div class="employee_container__btn primary__btn">
+        <router-link to="/new-employee" class="employee_container__add-employee-link">Add Employee</router-link>
+      </div>
     </header>
-    <section class="employee-container__table">
-    <a href="">Yo</a>
-    <a href="">Yo</a>
-    <a href="">Yo</a>
-    <a href="">Yo</a>
-    <a href="">Yo</a>
-    <a href="">Yo</a>
-    <a href="">Yo</a>
+    <section v-if="" class="employee-container__table" >
+      <a href="">Yo</a>
+      <a href="">Yo</a>
+      <a href="">Yo</a>
+      <a href="">Yo</a>
+      <a href="">Yo</a>
+      <a href="">Yo</a>
+      <a href="">Yo</a>
     </section>
   </section>
 </template>
