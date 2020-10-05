85e9317b0c0475e363655a02cfeba4c670b0069a
giselle2323 <yakubuaminat94@gmail.com>
2020-10-05 18:43:42 +0100

    feat:nedit, sidebar, nav components

---
+++ resources/components/InnerContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="inner-container">
+    <section class="employee__section">
+      <SideBar />
+      <router-view />
+    </section>
+  </section>
+</template>
+
+<script>
+import SideBar from "./SideBar.vue";
+import EmployeesContainer from "./EmployeesContainer.vue";
+
+export default {
+  name: "inner-component",
+  components: {
+    SideBar,
+    EmployeesContainer,
+  },
+  props: {
+    employees: {
+      type: Array,
+    },
+  },
+};
+</script>
--- resources/components/NavBar.vue
+++ resources/components/NavBar.vue
@@ -2,20 +2,49 @@
   <div class="nav-bar">
     <header class="nav__header">
       <section class="logo__section">
-        <a href="/"><img src="../assets/images/logo.svg" alt="logo" height="50px" width="180px"></a>
+        <a href="/"
+          ><img
+            src="../assets/images/logo.svg"
+            alt="logo"
+            height="60px"
+            width="192px"
+        /></a>
       </section>
       <section class="nav-menu__section">
-        <a href="/">Calendar</a>
-        <a href="/">Statistic</a>
-        <a href="/">Employee</a>
-        <a href="">Client</a>
-        <a href="/">Equipment</a>
+        <div class="nav-link-container">
+          <a href="/">Calendar</a>
+          <a href="/">Statistic</a>
+          <a href="/" class="active-link">Employee</a>
+          <a href="/">Client</a>
+          <a href="/">Equipment</a>
+        </div>
       </section>
       <section class="nav-notifications__section">
-        <button class="notification__btn"><img src="../assets/images/notifications.png" alt="notifications" width="45px" height="45px"></button>
-        <button class="message__btn"><img src="../assets/images/message.png" alt="messages" width="45px" height="45px"></button>
-        <div class="nav-image-container">
-          <img src="../assets/images/message.png" alt="" srcset="">
+        <div class="nav-notifications-inner-container">
+          <div class="notification-container">
+            <button class="notification__btn">
+              <img
+                src="../assets/images/notifications.png"
+                alt="notifications"
+                width="45px"
+                height="45px"
+              />
+            </button>
+          </div>
+          <div class="message__btn-container">
+            <div class="message-dot"></div>
+            <button class="message__btn">
+              <img
+                src="../assets/images/message.png"
+                alt="messages"
+                width="45px"
+                height="45px"
+              />
+            </button>
+          </div>
+          <div class="nav-image-container">
+            <img src="../assets/images/profile-head.png" alt="profile-image" srcset="" width="45px" height="45px"/>
+          </div>
         </div>
       </section>
     </header>
