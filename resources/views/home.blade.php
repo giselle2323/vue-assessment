7017adab7733a2c9e4c1f0fac6d3d9fd2d55cf94
giselle2323 <yakubuaminat94@gmail.com>
2020-10-05 18:45:04 +0100

    styles: added reset css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,8 @@
         <title>Employee Page</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="css/reset.css" rel="stylesheet" >
         <link href="{{ mix('css/app.css') }}" rel="stylesheet" >
     </head>
